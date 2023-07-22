38e8111babdc73cbc54d6f2c83f8e8197f7c2453
Dinuka Rathnayaka <112457556+Dinuka-Rathnayake@users.noreply.github.com>
2023-07-22 11:20:07 +0530

    Update AddIncomes.js

--- Mobile-Store-Management_System/src/front-end/src/layouts/finance/AddIncomes.js
+++ Mobile-Store-Management_System/src/front-end/src/layouts/finance/AddIncomes.js
@@ -1,7 +1,7 @@
 import React,{useState} from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-// import './Finance.css'
+import './Finance.css'
 import Swal from 'sweetalert2';
 
 

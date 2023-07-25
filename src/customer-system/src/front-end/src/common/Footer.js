02aa1b85c106c90f46834df49fa4fec6f44f7c49
Dinuka Rathnayaka <112457556+Dinuka-Rathnayake@users.noreply.github.com>
2023-07-26 01:03:07 +0530

    footer done

--- src/customer-system/src/front-end/src/common/Footer.js
+++ src/customer-system/src/front-end/src/common/Footer.js
@@ -43,9 +43,7 @@ function Footer (){
               <li>
                 <a href="#">Our Services</a>
               </li>
-              <li>
-                <a href="#">How to shop on Molla</a>
-              </li>
+              
               <li>
                 <a href="faq.html">FAQ</a>
               </li>
@@ -72,15 +70,11 @@ function Footer (){
               <li>
                 <a href="#">Returns</a>
               </li>
-              <li>
-                <a href="#">Shipping</a>
-              </li>
+            
               <li>
                 <a href="#">Terms and conditions</a>
               </li>
-              <li>
-                <a href="#">Privacy Policy</a>
-              </li>
+              
             </ul>
             {/* End .widget-list */}
           </div>
@@ -102,9 +96,6 @@ function Footer (){
                 <a href="#">My Wishlist</a>
               </li>
               <li>
-                <a href="#">Track My Order</a>
-              </li>
-              <li>
                 <a href="#">Help</a>
               </li>
             </ul>
@@ -124,10 +115,10 @@ function Footer (){
     {/* End .container */}
   </div>
   {/* End .footer-middle */}
-  <div className="footer-bottom">
+  <div className="footer-bottom ">
     <div className="container">
       <p className="footer-copyright pl-6">
-        ITP_WE_20
+        All rights reserved | 2023 | Dinuka Rathnayake | +94 77 619 5029
       </p>
       {/* End .footer-copyright */}
       {/* <figure className="footer-payments">
